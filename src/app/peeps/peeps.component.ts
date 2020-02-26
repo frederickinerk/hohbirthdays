c152bf43f1fd4b4dc116c19c24aa105bacd2f1a5
frederickinerk <dennis.j.higgins@gmail.com>
2020-02-26 20:11:06 +1100

    2 files modified, 3 added

---
+++ src/app/peeps/peeps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-peeps',
+  templateUrl: './peeps.component.html',
+  styleUrls: ['./peeps.component.css']
+})
+export class PeepsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
